848b3c7a3b75d943339b80980e7f87d1d7d80084
lyudmilaklochko <devonkalyu@gmail.com>
2022-01-31 17:37:49 +0200

    first commit

---
+++ min/script.js
@@ -0,0 +1,81 @@
+
+// let age = prompt('How old are you?');
+
+// if (age <= 11) {
+//   alert('child');
+// } else if (age <= 17) {
+//   alert('teenager');
+// } else if (age <= 59) {
+//   alert('adult');
+// } else {
+//   alert ('pensioner');
+// }
+
+// let num = prompt('Enter a number from 0 to 9')
+
+// switch (num) {
+//     case '1':
+//     alert('!');
+//     break;
+
+//     case '2':
+//     alert('@');
+//     break;
+    
+//     case '3':
+//     alert('#');
+//     break;
+    
+//     case '4':
+//     alert('$');
+//     break;
+
+//     case '5':
+//     alert('%');
+//     break;
+    
+//     case '6':
+//     alert ('^');
+//     break;
+    
+//     case '7':
+//     alert ('&');
+//     break;
+    
+//     case '8':
+//     alert ('*');
+//     break;
+    
+//     case '9':
+//     alert ('(');
+//     break;
+// }
+
+
+// let a = Number(prompt('Enter the beginning of the range'));
+// let n = Number(prompt('Enter the end of the range'));
+// let sum = 0;
+ 
+// while(a <= n){
+//    sum+=a;
+//     a++;
+// }
+ 
+// alert(sum)
+
+
+//   let num = prompt('Enter a number')
+//   let i = 0;
+//   while (i++ <= num)
+//   {
+//     if (num % i == 0)
+//    {
+//     alert(i);
+//     }
+//   }
+
+    
+   
+
+   
+
---
+++ normal/script.js
@@ -0,0 +1,25 @@
+// let day = 'Monday';
+
+// switch (day) {
+//     case 'Monday':
+//          alert('Monday. Do you want see next day?');
+
+//     case 'Tuesday':
+//          alert('Tuesday. Do you want see next day?');
+   
+//     case 'Wednesday':
+//          alert('Wednesday. Do you want see next day?');
+    
+//     case 'Thursday':
+//          alert('Thursday. Do you want see next day?');
+        
+//     case 'Friday':
+//          alert('Friday. Do you want see next day?');
+        
+//     case 'Saturday':
+//          alert('Saturday. Do you want see next day?');
+
+//     case 'Sunday':
+//          alert('Sunday. Do you want see next day?');
+//     break;
+// }
